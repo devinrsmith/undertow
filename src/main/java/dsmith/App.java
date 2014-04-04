0bb76b1c049f34e814ee88df4759cee11de51cdd
Devin Smith <dsmith@redcurrent.com>
2014-04-04 14:05:18 -0500

    added undertow

--- src/main/java/dsmith/App.java
+++ src/main/java/dsmith/App.java
@@ -1,13 +1,20 @@
 package dsmith;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import io.undertow.Undertow;
+import io.undertow.server.handlers.resource.FileResourceManager;
+
+import java.io.File;
+import java.io.IOException;
+
+import static io.undertow.Handlers.resource;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        final Undertow server = Undertow.builder()
+                .addHttpListener(Integer.parseInt(args[1]), "localhost")
+                .setHandler(resource(new FileResourceManager(new File(args[0]), 100))
+                        .setDirectoryListingEnabled(true))
+                .build();
+        server.start();
     }
 }
